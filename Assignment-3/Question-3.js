98d7e4e5549e3f57b79e93c19fbe3d24bae05cde
koushik-dasss <koushikdasmagnate2005@gmail.com>
2025-04-02 00:31:55 +0530

    Qs. 3 added , cel to far in js

---
+++ Assignment-3/Question-3.js
@@ -0,0 +1,5 @@
+// Write a JavaScript Function that will convert Celsius to Farhenheit. 
+let cel = 50 ; // cel = 10 degree celsius , user input also can be done 
+let f = (cel*9/5)+32;
+console.log(cel+ " degree  celcius is " + f + " degree farheinheit ");
+
