f124382bbde516ba79f2c08d55a95f3ff72b196c
koushik-dasss <koushikdasmagnate2005@gmail.com>
2025-04-09 00:00:09 +0530

    dumping all files

---
+++ Assignment-3/Question-10.js
@@ -0,0 +1,3 @@
+// Write a JavaScript program that functions as a simple calculator. The 
+// program should take two numbers and an arithmetic operator (such as +, -, 
+// *, /, or %) and perform the corresponding operation.
---
+++ Assignment-3/Question-5(optimized).js
@@ -0,0 +1,2 @@
+// Write a JavaScript function that finds the second largest and second 
+// smallest number in an array of numbers. 
